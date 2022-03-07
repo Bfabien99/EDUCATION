82895569ccb376ff636edf73502554256df54a7e
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-03-07 12:17:46 +0000

    first commit

---
+++ views/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>EDUCATION</h1>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatibus dolores minima culpa ex quis repellat voluptas tenetur expedita vitae!
+    </p>
+
+</body>
+</html>
